fe72b7f3fa1faacd700eb4685a856cab0b0683b2
ChooJosh <joshchoo@outlook.com>
2019-09-12 09:26:35 -0500

    added spawn script

---
+++ CS4361/Assets/Scripts/SpawnGroups.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnGroups : MonoBehaviour
+{
+	//array of game objects
+	public GameObject[] groups;
+
+	public void nextGroup()
+	{
+		int index = Random.Range(0, groups.Length);
+		Instantiate(groups[index], transform.position, Quaternion.identity);
+	}
+	// Start is called before the first frame update
+	void Start()
+    {
+		//start the 1st group
+		nextGroup();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
