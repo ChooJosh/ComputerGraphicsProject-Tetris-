4a3136ed0fe57fb22fdbb99473c196b1a75d75bb
Jonathan Lyttle <myncijub@gmail.com>
2019-10-14 17:13:22 -0500

    Added block movement script

---
+++ CS4361/Assets/BlockMovementController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockMovementController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.position += 5 * Vector3.left;
+        }
+        if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.position += 5 * Vector3.right;
+        }
+    }
+}
